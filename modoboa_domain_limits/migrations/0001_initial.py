d6c30c1ab34e611431601dfc182953ed4447ec12
Gael Pasgrimaud <gael@gawel.org>
2016-01-18 18:49:10 +0100

    reset migration

--- modoboa_domain_limits/migrations/0001_initial.py
+++ modoboa_domain_limits/migrations/0001_initial.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('admin', '0003_auto_20151118_1215'),
     ]
 
     operations = [
@@ -16,11 +17,10 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('mail_limit', models.IntegerField(default=-1)),
                 ('alias_limit', models.IntegerField(default=-1)),
-                ('domain', models.OneToOneField(to='modoboa.admin.Domain')),
+                ('domain', models.OneToOneField(to='admin.Domain')),
             ],
             options={
                 'db_table': 'domain_limits',
             },
-            bases=(models.Model,),
         ),
     ]
